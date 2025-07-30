6c4344c4d4b606bd8e87901a097546a6c5d84c4e
e-yee <viduydmm@gmail.com>
2025-07-31 00:50:16 +0700

    feat: update employee route

--- backend/src/app/routes/employee_route.py
+++ backend/src/app/routes/employee_route.py
@@ -1,14 +1,20 @@
 from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
+from sqlalchemy.exc import IntegrityError
 from extensions import db
+from ..auth import role_required
 from ..http_status import HTTPStatus
 from ..models import Employee
 from ..schemas.employee_schema import employee_schema, employees_schema
 
 employee_bp = Blueprint("employee_bp", __name__,  url_prefix="/employee")
 
-@employee_bp.post("/add")
-def add_employee(): 
+# General features
+
+# Manager features
+@employee_bp.post('/add')
+@role_required("Manager")
+def add_employee():
     try:
         if not request.is_json:
             return jsonify({
@@ -18,25 +24,42 @@ def add_employee():
         json_data = request.get_json()
         validated = employee_schema.load(json_data)
         
+        if validated["role"] == "Manager":
+            return jsonify({
+                "message": "Permission denied for adding manager"
+            }), HTTPStatus.FORBIDDEN
+        
         employee = Employee(
-            id = validated["id"],
-            full_name = validated["full_name"],
-            email = validated["email"],
-            role = validated["role"],
-            phone_number = validated["phone_number"],
-            teacher_status = validated["teacher_status"]
+            id=validated["id"],
+            full_name=validated["full_name"],
+            email=validated["email"],
+            role=validated["role"],
+            phone_number=validated["phone_number"],
+            teacher_status=validated["teacher_status"]
         )
         db.session.add(employee)
         db.session.commit()
-        return employee_schema.jsonify(employee), HTTPStatus.CREATED
-    
+        
+        return jsonify(employee_schema.dump(employee)), HTTPStatus.CREATED
+
     except ValidationError as ve:
         return jsonify({
-            "message": "Invalid input", 
+            "message": "Invalid input",
             "error": ve.messages
         }), HTTPStatus.BAD_REQUEST
-
-@employee_bp.get("/")
-def get_all():
-    employees = db.session.query(Employee).all()
-    return employees_schema.jsonify(employees), HTTPStatus.OK
+    
+    except IntegrityError as ie:
+        db.session.rollback()
+        return jsonify({
+            "message": "Violate database constraint",
+            "error": str(ie.orig)
+        }), HTTPStatus.BAD_REQUEST
+    
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({
+            "message": "Unexpected error occured",
+            "error": str(e)
+        }), HTTPStatus.INTERNAL_SERVER_ERROR
+        
+# Teacher features
