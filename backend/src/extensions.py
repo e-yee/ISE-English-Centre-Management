1817f6c169894ce2195a653de367e09c2c15988e
e-yee <viduydmm@gmail.com>
2025-08-05 17:51:00 +0700

    feat: add reset password

--- backend/src/extensions.py
+++ backend/src/extensions.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_cors import CORS
+from flask_mail import Mail
 from passlib.context import CryptContext
 
 ma = Marshmallow()
@@ -16,5 +17,7 @@ class Base(DeclarativeBase): pass
 db = SQLAlchemy(model_class=Base)
 
 cors = CORS()
+
+mail = Mail()
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
