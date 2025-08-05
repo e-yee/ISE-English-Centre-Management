68af8e328109c4dc7d5b6c98d423124741a1770b
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2025-08-05 19:29:35 +0700

    chore(schemas): fix teacher schema

--- backend/src/app/schemas/teacher/issue_schema.py
+++ backend/src/app/schemas/teacher/issue_schema.py
@@ -2,12 +2,12 @@ from extensions import ma
 from marshmallow import fields
 
 class IssueSchema(ma.Schema):
-    id = fields.String(required=True)
+    id = fields.String(required=False)
     teacher_id = fields.String(required=True)
     issue_type = fields.String(required=True)
     issue_description = fields.String(required=True)
-    status = fields.String(required=True)
-    reported_date = fields.Date(required=True)
+    status = fields.String(required=False)
+    reported_date = fields.Date(required=False)
     student_id = fields.String(required=False)
     room_id = fields.String(required=False)
 
