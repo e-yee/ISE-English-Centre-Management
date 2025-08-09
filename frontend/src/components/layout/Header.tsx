a8c5a6c14ad7f58f30522d8b83d6a4e06dbc1558
Thien Phu <truongcongthienphu1910@gmail.com>
2025-08-09 15:31:32 +0700

    Hide "How to use?"

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -102,7 +102,7 @@ const Header: React.FC<HeaderProps> = ({ isRegistered = false }) => {
       <div className="flex items-center pl-6 flex-1">
         <button
           onClick={handleHowToUseClick}
-          className="text-black text-[28px] font-normal font-comfortaa hover:underline transition-all duration-200 mt-2"
+          className="invisible text-black text-[28px] font-normal font-comfortaa hover:underline transition-all duration-200 mt-2"
         >
           How to use?
         </button>
